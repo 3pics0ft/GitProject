e3de8f87b29e3edbac707bb3ec3453e3da0cf534
Mick Piatin <developer@epicsoft.store>
2022-07-17 16:27:33 +0930

    Change link back to blue

--- GitProject/ContentView.swift
+++ GitProject/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 Text("This is the Content View")
             NavigationLink(destination: ModalView()) {
                 Text("Go Next")
-            }.foregroundColor(.purple)
+            }.foregroundColor(.blue)
             .navigationTitle("Xcode and Git")
             }
         }
