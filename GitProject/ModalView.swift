6d17dcc3ac2b86aa39f920d61c755ac6ac713869
Mick Piatin <developer@epicsoft.store>
2022-07-17 10:20:18 +0930

    Created separate modal view

---
+++ GitProject/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  GitProject
+//
+//  Created by Mike Piatin on 17/7/2022.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal view")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
